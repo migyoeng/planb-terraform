1e32a4a0943f6d76c4bd168a490a711afd56c133
hwiseokseo <bidoru4417@gmail.com>
2025-09-11 14:43:44 +0900

    eip 코드 변경

--- generated/aws/eip/eip.tf
+++ generated/aws/eip/eip.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "tfer--eipalloc-0f96659c79aaa38da" {
   domain               = "vpc"
   network_border_group = "ap-northeast-2"
-  network_interface    = "eni-0401f525b76d15fb1"
+  network_interface    = "eni-00eb6e76a8a9deb53"
   public_ipv4_pool     = "amazon"
   region               = "ap-northeast-2"
 }
