6c9203f52022242c4d1b2bf56982042c1831a659
hwiseokseo <bidoru4417@gmail.com>
2025-09-11 11:29:06 +0900

    network_interface 코드 수정

--- generated/aws/eni/outputs.tf
+++ generated/aws/eni/outputs.tf
@@ -1,79 +1,63 @@
-resource "aws_network_interface" "tfer--eni-01b6af54923d675fe" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-00258c4a6ac72c4ce_id
+output "eni_01b6af54923d675fe_id" {
+  value = aws_network_interface.tfer--eni-01b6af54923d675fe.id
 }
 
-resource "aws_network_interface" "tfer--eni-0360b6116e121ea9f" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-02ef09cd4e75ad48e_id
+output "eni_0360b6116e121ea9f_id" {
+  value = aws_network_interface.tfer--eni-0360b6116e121ea9f.id
 }
 
-resource "aws_network_interface" "tfer--eni-0401f525b76d15fb1" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-0b9a0280aa66167fe_id
+output "eni_0401f525b76d15fb1_id" {
+  value = aws_network_interface.tfer--eni-0401f525b76d15fb1.id
 }
 
-resource "aws_network_interface" "tfer--eni-0779d6c0e3a03df3c" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-02ef09cd4e75ad48e_id
+output "eni_0779d6c0e3a03df3c_id" {
+  value = aws_network_interface.tfer--eni-0779d6c0e3a03df3c.id
 }
 
-resource "aws_network_interface" "tfer--eni-07cd0fb55a213e389" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-0e08b5b05fdad2efc_id
+output "eni_07cd0fb55a213e389_id" {
+  value = aws_network_interface.tfer--eni-07cd0fb55a213e389.id
 }
 
-resource "aws_network_interface" "tfer--eni-07fa670621d4eb5c1" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-0e08b5b05fdad2efc_id
+output "eni_07fa670621d4eb5c1_id" {
+  value = aws_network_interface.tfer--eni-07fa670621d4eb5c1.id
 }
 
-resource "aws_network_interface" "tfer--eni-0942501e66bda79ab" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-02ef09cd4e75ad48e_id
+output "eni_0942501e66bda79ab_id" {
+  value = aws_network_interface.tfer--eni-0942501e66bda79ab.id
 }
 
-resource "aws_network_interface" "tfer--eni-0a2d851c88b4bf1e3" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-07e1138aac7e95c7c_id
+output "eni_0a2d851c88b4bf1e3_id" {
+  value = aws_network_interface.tfer--eni-0a2d851c88b4bf1e3.id
 }
 
-resource "aws_network_interface" "tfer--eni-0a3a273fba081bfaa" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-07e1138aac7e95c7c_id
+output "eni_0a3a273fba081bfaa_id" {
+  value = aws_network_interface.tfer--eni-0a3a273fba081bfaa.id
 }
 
-resource "aws_network_interface" "tfer--eni-0b80cb1982ab47830" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-0dcde6507114a320f_id
+output "eni_0b80cb1982ab47830_id" {
+  value = aws_network_interface.tfer--eni-0b80cb1982ab47830.id
 }
 
-resource "aws_network_interface" "tfer--eni-0ca221316507dbd45" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-0e08b5b05fdad2efc_id
+output "eni_0ca221316507dbd45_id" {
+  value = aws_network_interface.tfer--eni-0ca221316507dbd45.id
 }
 
-resource "aws_network_interface" "tfer--eni-0cabedf961b37080d" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-0dcde6507114a320f_id
+output "eni_0cabedf961b37080d_id" {
+  value = aws_network_interface.tfer--eni-0cabedf961b37080d.id
 }
 
-resource "aws_network_interface" "tfer--eni-0d79480c9e32504af" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-0b9a0280aa66167fe_id
+output "eni_0d79480c9e32504af_id" {
+  value = aws_network_interface.tfer--eni-0d79480c9e32504af.id
 }
 
-resource "aws_network_interface" "tfer--eni-0e145efeab1d8f0cb" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-02ef09cd4e75ad48e_id
+output "eni_0e145efeab1d8f0cb_id" {
+  value = aws_network_interface.tfer--eni-0e145efeab1d8f0cb.id
 }
 
-resource "aws_network_interface" "tfer--eni-0e7302e0797b46b20" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-0e08b5b05fdad2efc_id
+output "eni_0e7302e0797b46b20_id" {
+  value = aws_network_interface.tfer--eni-0e7302e0797b46b20.id
 }
 
-resource "aws_network_interface" "tfer--eni-0f2623d112abcce58" {
-  # ...existing code...
-  subnet_id = data.terraform_remote_state.subnet.outputs.aws_subnet_tfer--subnet-0e08b5b05fdad2efc_id
+output "eni_0f2623d112abcce58_id" {
+  value = aws_network_interface.tfer--eni-0f2623d112abcce58.id
 }
