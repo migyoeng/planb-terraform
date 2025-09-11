f5f8d9be31615fece1c3bdd58d9abb9862698c40
hwiseokseo <bidoru4417@gmail.com>
2025-09-11 15:01:02 +0900

    nat 코드 변경

--- generated/aws/nat/nat_gateway.tf
+++ generated/aws/nat/nat_gateway.tf
@@ -1,9 +1,9 @@
 resource "aws_nat_gateway" "tfer--nat-0d1d1ba9ec02d3aa6" {
-  allocation_id                      = "eipalloc-0f96659c79aaa38da"
+  allocation_id                      = "eipalloc-0162dace52f5a9647"
   connectivity_type                  = "public"
   private_ip                         = "10.100.1.153"
   region                             = "ap-northeast-2"
-  subnet_id                          = "subnet-0b9a0280aa66167fe"
+  subnet_id                          = "subnet-04571057be0dbb468"
 
   tags = {
     Name = "DugOut-NAT"
